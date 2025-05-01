679856501eaadb6354da9618bc77dbc2a4a009fb
sreesha1502 <sreesha1523@gmail.com>
2025-05-01 16:16:32 +0200

    project artifact and groupid update

--- src/main/java/com/vegData/kafka_mongodb/KafkaMongodbApplication.java
+++ src/main/java/com/vegData/kafka_mongodb/KafkaMongodbApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.vegData.kafka_mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class KafkaMongodbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(KafkaMongodbApplication.class, args);
 	}
 
 }
